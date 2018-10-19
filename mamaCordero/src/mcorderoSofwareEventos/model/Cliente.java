5e2d67709c5edee7ee8bd044b514d1348e1f3790
Flavio Toro B <38701988+fulkesko@users.noreply.github.com>
2018-10-19 18:18:42 -0300

    ajuste
    error en subia de archivos

--- mamaCordero/src/mcorderoSofwareEventos/model/Cliente.java
+++ mamaCordero/src/mcorderoSofwareEventos/model/Cliente.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mcorderoSofwareEventos.model;
 
-/**
- *
- * @author Flavio
- */
 public class Cliente {
+    private int id;
+    private String nombre;
+    private String run;
+    private String direccion;
+    private String telefono;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
 }
--- mamaCordero/src/mcorderoSofwareEventos/model/Servicio.java
+++ mamaCordero/src/mcorderoSofwareEventos/model/Servicio.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mcorderoSofwareEventos.model;
 
-/**
- *
- * @author Flavio
- */
 public class Servicio {
-    
+    private int id;
+    private String tipo_servicio;
+    private int costo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo_servicio() {
+        return tipo_servicio;
+    }
+
+    public void setTipo_servicio(String tipo_servicio) {
+        this.tipo_servicio = tipo_servicio;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
 }
